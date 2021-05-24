b8c3d72d7c4a7233b2db50b7dbb4938ff1a18782
Amine <aminebennani25@gmail.com>
2021-05-24 20:55:16 +0200

    New classes

---
+++ src/classes/compte/Courant.py
@@ -0,0 +1,24 @@
+from .Compte import Compte
+
+
+class Courant(Compte):
+
+    def __init__(self, numCompte, solde):
+        Compte.__init__(self, numCompte, solde)
+
+    def debiter(self, montant: int) -> None:
+        if isinstance(montant, (float, int)):
+            if self.solde >= montant:
+                self.solde -= montant
+            else:
+                raise Exception("Account balance is Null")
+        else:
+            raise Exception("TypeError")
+
+    def crediter(self, montant: int) -> None:
+        if isinstance(montant, (float, int)):
+            self.solde += montant
+        else:
+            raise Exception("TypeError")
+
+
---
+++ src/classes/compte/Epargne.py
@@ -0,0 +1,32 @@
+from .Compte import Compte
+
+
+class Epargne(Compte):
+
+    def __init__(self, taux: int, numCompte: int, solde: int):
+        Compte.__init__(self, numCompte, solde)
+        self.taux = taux
+
+    def setTaux(self, taux: int):
+        self.taux = taux
+
+    def getTaux(self) -> int:
+        return self.taux
+
+    def calculInterets(self) -> float:
+        return self.solde * self.taux / 100
+
+    def GetNewSolde(self) -> float:
+        self.solde += self.calculInterets()
+        return self.solde
+
+    def debiter(self, montant: int):
+        pass
+
+    def crediter(self, montant: int):
+        pass
+
+
+
+
+
